501cbbe28fcbcb706620216a550550513ca5df41
maib07001 <maib07001@E-PW05C37V>
2024-11-25 20:48:27 +0100

    Add project files.

---
+++ uppgift.3.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace inl_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int hourlyRate = 80;
+            const int maxDailyRate = 950;
+
+            Console.WriteLine("hur mänga timmer vill du hyra bilen? (max 24 timmar): ");
+            int hours;
+
+            if (int.TryParse(Console.ReadLine(), out hours) && hours > 0 && hours <= 24)
+            {
+                int totalcost = hours * hourlyRate;
+                if (totalcost > maxDailyRate)
+                {
+                    totalcost = maxDailyRate;
+                }
+
+                Console.WriteLine($"konstnaden för att hyra bilen i {hours} timmar är {totalcost}.");
+
+            }
+            else
+            {
+
+                Console.WriteLine("ange ett giltigt antal timmar mellan 1 och 24. ");
+            }
+        }
+    }
+}
